12fbf3e63fe5f73a095242c056a3d8599ab9c32b
Vishal Mathur <vishmathur5@gmail.com>
2014-02-22 14:00:59 -0500

    added another file doe

--- mobile/DealMe/src/com/vajy/dealme/Main.java
+++ mobile/DealMe/src/com/vajy/dealme/Main.java
@@ -3,13 +3,32 @@ package com.vajy.dealme;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Main extends Activity {
+	
+	private Button getDealsButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		getDealsButton = (Button)findViewById(R.id.getdeals);
+		getDealsButton.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View arg0) {
+				System.out.println("button clicked");
+				// TODO Auto-generated method stub
+				
+			}
+		});
+
+		
+		
 	}
 
 	@Override
@@ -18,5 +37,7 @@ public class Main extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	
 
 }
