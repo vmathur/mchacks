9cc776c58d445fc7e674af92b57bd17ade1c52d9
Yannick <yannickngana@gmail.com>
2014-02-23 09:11:39 -0500

    list works

--- mobile/DealMe/gen/com/vajy/dealme/R.java
+++ mobile/DealMe/gen/com/vajy/dealme/R.java
@@ -36,7 +36,7 @@ public final class R {
         public static final int getdeals=0x7f080004;
         public static final int linearLayout1=0x7f080005;
         public static final int linearLayout2=0x7f080007;
-        public static final int mainListView=0x7f08000b;
+        public static final int list=0x7f08000b;
         public static final int numberPicker1=0x7f080002;
         public static final int rowTextView=0x7f08000c;
         public static final int textView1=0x7f080000;
--- mobile/DealMe/src/com/vajy/dealme/Main.java
+++ mobile/DealMe/src/com/vajy/dealme/Main.java
@@ -17,7 +17,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import java.util.*;
-import java.util.HashMap; 
 
 import android.content.Intent;
 
@@ -34,7 +33,10 @@ import android.content.Context;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.NumberPicker;
 import android.widget.Button;
 import android.widget.TextView;
@@ -84,12 +86,72 @@ public class Main extends Activity implements LocationListener {
 			public void onClick(View arg0) {
 				System.out.println("button clicked");
 				setContentView(R.layout.result_page);
+				
+				final ListView listview = (ListView) findViewById(R.id.list);
+			    String[] values = new String[] { "Subway", "Zara", "WindowsMobile",
+			        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+			        "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+			        "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+			        "Android", "iPhone", "WindowsMobile" };
+
+			    final ArrayList<String> list = new ArrayList<String>();
+			    for (int i = 0; i < values.length; ++i) {
+			      list.add(values[i]);
+			    }
+			    final StableArrayAdapter adapter = new StableArrayAdapter(Main.this,
+			        android.R.layout.simple_list_item_1, list);
+			    listview.setAdapter(adapter);
+				
+			    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			        @Override
+			        public void onItemClick(AdapterView<?> parent, final View view,
+			            int position, long id) {
+			          final String item = (String) parent.getItemAtPosition(position);
+			          view.animate().setDuration(2000).alpha(0)
+			              .withEndAction(new Runnable() {
+			                @Override
+			                public void run() {
+			                  list.remove(item);
+			                  adapter.notifyDataSetChanged();
+			                  view.setAlpha(1);
+			                }
+			              });
+			        }
+
+			      });
+			    
 				//accept int getRadius.getValue() to set as radius
 				new Request().execute(); 
 			}
 		});
 	}
 	
+	
+	private class StableArrayAdapter extends ArrayAdapter<String> {
+
+	    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+	    public StableArrayAdapter(Context context, int textViewResourceId,
+	        List<String> objects) {
+	      super(context, textViewResourceId, objects);
+	      for (int i = 0; i < objects.size(); ++i) {
+	        mIdMap.put(objects.get(i), i);
+	      }
+	    }
+
+	    @Override
+	    public long getItemId(int position) {
+	      String item = getItem(position);
+	      return mIdMap.get(item);
+	    }
+
+	    @Override
+	    public boolean hasStableIds() {
+	      return true;
+	    }
+
+	  }
 	
 	public static void display(JSONObject newDeals) throws ParserConfigurationException, SAXException, IOException, JSONException{
 		
