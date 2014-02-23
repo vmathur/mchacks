cec284817d34c9a2df7b263f071e56391fbe09a0
whatarthurcodes <arthur.yan3@gmail.com>
2014-02-23 03:26:06 -0500

    attempted list
    check javavscript

---
+++ mobile/DealMe/src/com/vajy/dealme/List.java
@@ -0,0 +1,45 @@
+package com.vajy.dealme;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class List extends ListActivity {
+
+	static final String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
+			"Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
+			"Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" };
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		 System.out.println("Running.");
+
+		// no more this
+		// setContentView(R.layout.list_fruit);
+
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.result_page,
+				FRUITS));
+
+		ListView listView = getListView();
+		listView.setTextFilterEnabled(true);
+
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				 System.out.println("Running More");
+				// When clicked, show a toast with the TextView text
+				Toast.makeText(getApplicationContext(),
+						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+			}
+		});
+
+	}
+
+}
