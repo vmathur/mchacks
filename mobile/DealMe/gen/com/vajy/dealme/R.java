7a5ed8a08fa75479c0f78940d985dd2f0a92704d
whatarthurcodes <arthur.yan3@gmail.com>
2014-02-23 03:49:13 -0500

    another revision of lists

--- mobile/DealMe/gen/com/vajy/dealme/R.java
+++ mobile/DealMe/gen/com/vajy/dealme/R.java
@@ -31,18 +31,21 @@ public final class R {
         public static final int TextView03=0x7f080008;
         public static final int TextView04=0x7f080009;
         public static final int TextView05=0x7f080006;
-        public static final int action_settings=0x7f08000b;
+        public static final int action_settings=0x7f08000d;
         public static final int editText1=0x7f08000a;
         public static final int getdeals=0x7f080004;
         public static final int linearLayout1=0x7f080005;
         public static final int linearLayout2=0x7f080007;
+        public static final int mainListView=0x7f08000b;
         public static final int numberPicker1=0x7f080002;
+        public static final int rowTextView=0x7f08000c;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int main=0x7f030001;
         public static final int result_page=0x7f030002;
+        public static final int simplerow=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- mobile/DealMe/src/com/vajy/dealme/List.java
+++ mobile/DealMe/src/com/vajy/dealme/List.java
@@ -1,45 +1,46 @@
 package com.vajy.dealme;
 
-import android.app.ListActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
-
-public class List extends ListActivity {
-
-	static final String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
-			"Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
-			"Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" };
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		 System.out.println("Running.");
-
-		// no more this
-		// setContentView(R.layout.list_fruit);
-
-		setListAdapter(new ArrayAdapter<String>(this, R.layout.result_page,
-				FRUITS));
-
-		ListView listView = getListView();
-		listView.setTextFilterEnabled(true);
-
-		listView.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				 System.out.println("Running More");
-				// When clicked, show a toast with the TextView text
-				Toast.makeText(getApplicationContext(),
-						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
-			}
-		});
-
-	}
-
-}
+import java.util.ArrayList;  
+import java.util.Arrays;  
+  
+import android.app.Activity;  
+import android.os.Bundle;  
+import android.widget.ArrayAdapter;  
+import android.widget.ListView;  
+  
+public class List extends Activity {  
+    
+  private ListView mainListView ;  
+  private ArrayAdapter<String> listAdapter ;  
+    
+  /** Called when the activity is first created. */  
+  @Override  
+  public void onCreate(Bundle savedInstanceState) {  
+    super.onCreate(savedInstanceState);  
+    setContentView(R.layout.main);  
+      
+    // Find the ListView resource.   
+    mainListView = (ListView) findViewById( R.id.mainListView );  
+  
+    // Create and populate a List of planet names.  
+    String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",  
+                                      "Jupiter", "Saturn", "Uranus", "Neptune"};    
+    ArrayList<String> planetList = new ArrayList<String>();  
+    planetList.addAll( Arrays.asList(planets) );  
+      
+    // Create ArrayAdapter using the planet list.  
+    listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, planetList);  
+      
+    // Add more planets. If you passed a String[] instead of a List<String>   
+    // into the ArrayAdapter constructor, you must not add more items.   
+    // Otherwise an exception will occur.  
+    listAdapter.add( "Ceres" );  
+    listAdapter.add( "Pluto" );  
+    listAdapter.add( "Haumea" );  
+    listAdapter.add( "Makemake" );  
+    listAdapter.add( "Eris" );  
+      
+    // Set the ArrayAdapter as the ListView's adapter.  
+    mainListView.setAdapter( listAdapter );        
+  }  
+} 
